585e8d9737d1516d1607844e9e5bb50114bafcb2
nic0mp <nico.mp215@gmail.com>
2022-03-31 23:20:35 -0500

    Navbar update: added sections

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -6,7 +6,16 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt="Logo Image" />
+            <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
         </div>
     </div>
   )
