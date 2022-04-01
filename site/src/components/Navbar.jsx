6061a35cf5dc1e5fb185e467e27c5c39a247e743
nic0mp <nico.mp215@gmail.com>
2022-03-31 23:09:40 -0500

    update

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" />
+        </div>
+    </div>
   )
 }
 
