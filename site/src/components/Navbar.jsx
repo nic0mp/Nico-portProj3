53534182d7ee2e050710c1d3b793a8fafeb5e156
nic0mp <nico.mp215@gmail.com>
2022-04-06 15:47:11 -0500

    nav update

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -22,12 +22,12 @@ const Navbar = () => {
           </ul>        
 
         {/* Hamburger */}
-        <div className='md:hidden z-10'>
-          <FaBars />
+        <div onClick={handleClick} className='md:hidden z-10'>
+          {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
         {/* Mobile Menu */}
-        <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
           <li className='py-6 text-4xl'>Home</li>
           <li className='py-6 text-4xl'>About</li>
           <li className='py-6 text-4xl'>Skills</li>
