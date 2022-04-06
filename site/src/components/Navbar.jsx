8de3d34f76d73253bbcce9594279ac3796ce4ab5
nic0mp <nico.mp215@gmail.com>
2022-04-06 11:34:42 -0500

    nav update

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -24,7 +27,7 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu */}
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
           <li className='py-6 text-4xl'>Home</li>
           <li className='py-6 text-4xl'>About</li>
           <li className='py-6 text-4xl'>Skills</li>
