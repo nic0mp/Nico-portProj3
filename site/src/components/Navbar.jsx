b4f58b53db44ac94be7f6cbce45c4ea2f9c5d4d2
nic0mp <nico.mp215@gmail.com>
2022-04-08 13:41:45 -0500

    Navvy update

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -40,9 +40,10 @@ const Navbar = () => {
         {/* Social */}
         <div className='flex fixed flex-col top-[35%] left-0'>
           <ul>
-            <li className='w-[160px] h-[60px] flex justify-between items-center'>
-              <a href="/">
-                Linkedin <FaFacebook />
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+              <a className='flex justify-between items-center w-full text-gray-300'
+              href="/">
+                Linkedin <FaLinkedin size={30} />
               </a>
             </li>
           </ul>
