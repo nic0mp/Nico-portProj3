d450b826bf4dac5acae17a8ab44062dba4c59253
nic0mp <nico.mp215@gmail.com>
2022-04-10 21:16:39 -0500

    nav update

--- site/src/components/Home.jsx
+++ site/src/components/Home.jsx
@@ -4,7 +4,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
         <Navbar />
         <Home />
     </div>
