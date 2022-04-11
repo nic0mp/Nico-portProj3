43e3adc902766a1235da7d277eda205cf9a871f7
nic0mp <nico.mp215@gmail.com>
2022-04-10 22:58:09 -0500

    added copy

--- site/src/components/Home.jsx
+++ site/src/components/Home.jsx
@@ -4,9 +4,15 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
+   
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-        <Navbar />
-        <Home />
+        
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p> Hi</p>
+            <h1>Nico Pinguel</h1>
+        </div>
+
     </div>
   )
 }
