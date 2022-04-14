a441bc6b4b79a30f3a4deee678488c053297be44
nic0mp <nico.mp215@gmail.com>
2022-04-14 15:40:00 -0500

    view work button update

--- site/src/components/Home.jsx
+++ site/src/components/Home.jsx
@@ -22,10 +22,10 @@ const Home = () => {
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-4" />
+            </span>
           </button>
-          <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-4" />
-          </span>
         </div>
       </div>
     </div>
