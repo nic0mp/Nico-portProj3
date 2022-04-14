01bc0261eba8561fc095d7f2eee374db424f30ca
nic0mp <nico.mp215@gmail.com>
2022-04-14 16:29:52 -0500

    create about section

---
+++ site/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screenbg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div>
+          <p>About</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
