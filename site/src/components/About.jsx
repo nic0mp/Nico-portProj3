22a2a5255186c61fbb54594bad071f37dc772982
nic0mp <nico.mp215@gmail.com>
2022-04-19 21:48:41 -0500

    update

--- site/src/components/About.jsx
+++ site/src/components/About.jsx
@@ -8,12 +8,16 @@ const About = () => {
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
           </div>
-          <div>
-
-          </div>
-        <div>
-          
+          <div></div>
         </div>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div>
+            <p>Hi, I'm Nico very pleased to meet you. Feel free to look around</p>
+          </div>
+          <div>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut </p>
+          </div>
         </div>
       </div>
     </div>
