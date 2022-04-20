0bc2c917b3f070b80d3b6ac493cf9c52b03c618b
nic0mp <nico.mp215@gmail.com>
2022-04-20 15:40:04 -0500

    update

--- site/src/components/Skills.jsx
+++ site/src/components/Skills.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import Github from '../assets/github.png';
 
 const Skills = () => {
   return (
