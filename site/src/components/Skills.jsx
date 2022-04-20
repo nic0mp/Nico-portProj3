7d32ee7cf388e49177fef7b886fb63593167aa29
nic0mp <nico.mp215@gmail.com>
2022-04-20 15:29:33 -0500

    skills section

---
+++ site/src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
