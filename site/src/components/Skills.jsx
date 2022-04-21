7190df57c6f44aefc22f4185323801b29d771b7f
nic0mp <nico.mp215@gmail.com>
2022-04-21 13:48:11 -0500

    update

--- site/src/components/Skills.jsx
+++ site/src/components/Skills.jsx
@@ -5,10 +5,15 @@ import CSS from '../assets/css.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import Github from '../assets/github.png';
+import JavaScript from '../assets/javascript.png';
+import Tailwind from '../assets/tailwind.png';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills'>
+        {/*container*/}
+
+    </div>
   )
 }
 
