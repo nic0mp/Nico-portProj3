e0800ccc7011e6d26f4c3e3f90c21b0f16501265
nic0mp <nico.mp215@gmail.com>
2022-04-25 15:31:43 -0500

    update

--- site/src/components/Skills.jsx
+++ site/src/components/Skills.jsx
@@ -15,11 +15,11 @@ const Skills = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-orange-500'>Experience</p>
-                <p>These are the technologies I've worked with</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
             </div>
 
-            <div>
-                <div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                     <p>HTML</p>
                 </div>
