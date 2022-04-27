b6b25f0bc01d82f8a7942530e27603615c2028c8
nic0mp <nico.mp215@gmail.com>
2022-04-27 14:48:13 -0500

    update works

--- site/src/components/Work.jsx
+++ site/src/components/Work.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div name='work '>
-        
+    <div name='work' className='w-full md-h-screen text-gray-300 bg-[#0a192f]'>
+        <div>
+            <p></p>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <span>
+
+                    </span>
+                    <div>
+                        <a href="/">
+                            <button></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
