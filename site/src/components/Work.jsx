3c8a3437d04528df21adda73e33ec2924e785e81
nic0mp <nico.mp215@gmail.com>
2022-05-02 15:44:11 -0500

    update grid

--- site/src/components/Work.jsx
+++ site/src/components/Work.jsx
@@ -4,7 +4,7 @@ import realEstate from '../assets/realestate.jpg'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md-h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
@@ -12,10 +12,8 @@ const Work = () => {
             </div>
 
             {/* Container */}
-            <div 
-            
-            className='grid sm:grid-cols-2 md-cols-3 gap-4'>
-
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+    
                 {/* Grid Item */}
                 <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
@@ -26,27 +24,10 @@ const Work = () => {
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 text-gray-700 font-bold text-lg'>Demo</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/*Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span  className='text-2xl font-bold text-white tracking-wider'>
-    React JS Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 text-gray-700 font-bold text-lg'>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
