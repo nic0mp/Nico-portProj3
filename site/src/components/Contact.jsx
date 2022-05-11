74818f1729752eb06c7abe805e8f94ba57de5cf0
nic0mp <nico.mp215@gmail.com>
2022-05-11 15:37:56 -0500

    update

--- site/src/components/Contact.jsx
+++ site/src/components/Contact.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-orange-500'>Contact</p>
+            </div>
+        </form>
+    </div>
   )
 }
 
